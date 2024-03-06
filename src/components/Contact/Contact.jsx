5a505198abaf100c2e1e088d42a428cceab8443f
Emekaugo <36327185+Emekaugo@users.noreply.github.com>
2024-03-06 21:21:33 +0100

    uploaded portfolio

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@email.com">emyugochukwu@gmail.com.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/chukwuemeka-ugochukwu-a98b22156">
+            www.linkedin.com/in/chukwuemeka-ugochukwu-a98b22156
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/myname">github.com/Emekaugo</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
